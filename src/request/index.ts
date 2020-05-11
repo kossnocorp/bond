da4d755873367011f40cf9016243a466364d6210
Sasha Koss <koss@nocorp.me>
2020-05-11 15:05:06 +0400

    Adjust request to accept query, body and params

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,22 +1,44 @@
 import { Endpoint } from '../endpoint'
 
-export function request<Response, Params>(
-  endpoint: Endpoint<Response, Params>
-): Params extends undefined
-  ? () => Promise<Response>
-  : (params: Params) => Promise<Response> {
-  // @ts-ignore
-  return async (params) => {
+type EndpointPayload<Query, Body, Params> = (Query extends undefined
+  ? { query?: undefined }
+  : Partial<Query> extends Query
+  ? { query?: Query | undefined }
+  : { query: Query }) &
+  (Body extends undefined
+    ? { body?: undefined }
+    : Partial<Body> extends Body
+    ? { body?: Body | undefined }
+    : { body: Body }) &
+  (Params extends undefined
+    ? { params?: undefined }
+    : Partial<Params> extends Params
+    ? { params?: Params | undefined }
+    : { params: Params })
+
+type EndpointRequest<Response, Query, Body, Params> = Partial<
+  EndpointPayload<Query, Body, Params>
+> extends EndpointPayload<Query, Body, Params>
+  ? (payload?: EndpointPayload<Query, Body, Params>) => Promise<Params>
+  : (payload: EndpointPayload<Query, Body, Params>) => Promise<Response>
+
+export function request<Response, Query, Body, Params>(
+  endpoint: Endpoint<Response, Query, Body, Params>
+): EndpointRequest<Response, Query, Body, Params> {
+  return (async (payload: EndpointPayload<Query, Body, Params> | undefined) => {
+    const query = payload?.params || ({} as Params)
+    const body = payload?.params || ({} as Params)
+    const params = payload?.params || ({} as Params)
+
     const response = await fetch(endpoint.path(params), {
       method: endpoint.method,
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
-      // @ts-ignore
-      body: JSON.stringify(params?.body),
+      body: JSON.stringify(payload?.body),
     })
     const json = await response.json()
     return json as Response
-  }
+  }) as EndpointRequest<Response, Query, Body, Params>
 }
--- test/system/client.ts
+++ test/system/client.ts
@@ -1,7 +1,14 @@
 import api from './api'
 import { request } from '../../src/request'
 
+request(api.books)({ query: { filter: 'unread' } }).then(console.log)
+
 request(api.books)().then(console.log)
+
 request(api.book)({
-  bookId: 'harry-potter-and-the-sorcerer-s-stone',
+  params: {
+    bookId: 'harry-potter-and-the-sorcerer-s-stone',
+  },
 }).then(console.log)
+
+request(api.book)({}).then(console.log)
